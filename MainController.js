63e0d86e7f4ffd72014f382b2252a62179902fff
Liz Chapman <Lizjdavenport@gmail.com>
2017-11-02 16:43:35 -0400

    Group Project To Do List

---
+++ MainController.js
@@ -0,0 +1,20 @@
+var app = angular.module('myApp', []);
+app.controller('MainCtrl', function($scope){
+    
+    $scope.chores = ['wash dishes','buy groceries', 'pay bills', "fold laundry"];
+    
+    
+//    
+//    $scope.addchore = function(){
+//        $scope.chores.unshift({
+//            $scope.chore
+//        });
+//    }
+    
+    
+    
+});
+
+
+
+
